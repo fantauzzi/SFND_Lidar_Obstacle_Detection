a197468e9ae301c7917ccfdf543b13eece51ae0b
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2020-04-27 16:40:31 +0200

    Reading of point cloud from disk stuck

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -7,7 +7,6 @@
 // using templates for processPointClouds so also include .cpp to help linker
 #include "../../processPointClouds.cpp"
 #include <random>
-#include <cassert>
 #include <cmath>
 #include <algorithm>
 
@@ -47,7 +46,8 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData() {
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D() {
     ProcessPointClouds<pcl::PointXYZ> pointProcessor;
-    return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
+    return pointProcessor.loadPcd("/home/fanta/workspace/SFND_Lidar_Obstacle_Detection/src/sensors/data/pcd");
+    // return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
 }
 
 
@@ -61,20 +61,87 @@ pcl::visualization::PCLVisualizer::Ptr initScene() {
 }
 
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol) {
-    // std::unordered_set<int> inliersResult;
     srand(time(NULL));
 
     // DONE: Fill in this function
 
+    // Used to sample points from the cloud
     std::default_random_engine generator;
     std::uniform_int_distribution<int> distribution(0, cloud->size() - 1);
 
+    /* Keep a set with the best result so far, and a separate working set. Will swap between the two when the
+     * working set turns out to be better than the other set; it prevents unnecessary copying of the set.
+     * Note: an alternative viable implementation is to keep track of the best set so far by storing only its
+     * two representative points, as opposed to the whole set of inliners. */
+    std::unordered_set<int> inliersResult1;
+    std::unordered_set<int> inliersResult2;
+    std::unordered_set<int> &working = inliersResult1;
+    std::unordered_set<int> &best = inliersResult2;
+
+    for (int iter = 0; iter < maxIterations; ++iter) {
+        std::cout << "Iteration# " << iter << std::endl;
+        // Fetch two random points from the cloud, without repetition (repetition would produce a division by zero below)
+        int pick = distribution(generator);
+        int second_pick = pick;
+        int third_pick = pick;
+        while (second_pick == pick)
+            second_pick = distribution(generator);
+        while (third_pick == pick || third_pick == second_pick)
+            third_pick = distribution(generator);
+
+        auto x1 = cloud->points[pick].x;
+        auto y1 = cloud->points[pick].y;
+        auto z1 = cloud->points[pick].z;
+        auto x2 = cloud->points[second_pick].x;
+        auto y2 = cloud->points[second_pick].y;
+        auto z2 = cloud->points[second_pick].z;
+        auto x3 = cloud->points[third_pick].x;
+        auto y3 = cloud->points[third_pick].y;
+        auto z3 = cloud->points[third_pick].z;
+        auto i = (y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1);
+        auto j = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1);
+        auto k = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+        auto a = i;
+        auto b = j;
+        auto c = k;
+        auto d = -(i * x1 + j * y1 + k * z1);
+        auto denominator = sqrt(a * a + b * b + c * c);
+        // Build the set of inliners for the two sample points and the given max distance
+        for (int point_idx = 0; point_idx < cloud->size(); ++point_idx) {
+            auto point = (*cloud)[point_idx];
+            auto dist = abs(a * point.x + b * point.y + c * point.z + d) / denominator;
+            if (dist <= distanceTol)
+                working.emplace(point_idx);
+        }
+        // Keep track of the best (iter.e. biggest) set of inliners so far
+        if (working.size() > best.size())
+            std::swap(working, best);
+        working.clear();
+    }
+    return best;
+
+}
+
+std::unordered_set<int> RansacLine(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol) {
+    srand(time(NULL));
+
+    // DONE: Fill in this function
+
+    // Used to sample points from the cloud
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(0, cloud->size() - 1);
+
+    /* Keep a set with the best result so far, and a separate working set. Will swap between the two when the
+     * working set turns out to be better than the other set; it prevents unnecessary copying of the set.
+     * Note: an alternative viable implementation is to keep track of the best set so far by storing only its
+     * two representative points, as opposed to the whole set of inliners. */
     std::unordered_set<int> inliersResult1;
     std::unordered_set<int> inliersResult2;
     std::unordered_set<int> &working = inliersResult1;
     std::unordered_set<int> &best = inliersResult2;
 
     for (int i = 0; i < maxIterations; ++i) {
+        // Fetch two random points from the cloud, without repetition (repetition would produce a division by zero below)
         int pick = distribution(generator);
         int second_pick = pick;
         while (second_pick == pick)
@@ -87,12 +154,14 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
         auto b = x2 - x1;
         auto c = x1 * y2 - x2 * y1;
         auto denominator = sqrt(a * a + b * b);
+        // Build the set of inliners for the two sample points and the given max distance
         for (int j = 0; j < cloud->size(); ++j) {
             auto point = (*cloud)[j];
             auto d = abs(a * point.x + b * point.y + c) / denominator;
             if (d <= distanceTol)
                 working.emplace(j);
         }
+        // Keep track of the best (i.e. biggest) set of inliners so far
         if (working.size() > best.size())
             std::swap(working, best);
         working.clear();
@@ -101,13 +170,14 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 
 }
 
+
 int main() {
 
     // Create viewer
     pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
     // Create data
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 
 
     // DONE: Change the max iteration and distance tolerance arguments for Ransac function
